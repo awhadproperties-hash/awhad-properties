4ae9ff5290ab864dfffcef7f80e7180daec3b4d8
Thamir <thamirsiddik@gmail.com>
2024-04-26 22:09:35 +0530

    Initial commit: Setting up the foundation for the Apple iPhone 15 website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+export default function App() {
+  return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </main>
+  )
+}
