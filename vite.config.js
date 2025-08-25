045475fd5df928c9f5627893d4b27c889d1ad223
Mohammed-Farhaan-N <mohammedfarhaan235@gmail.com>
2025-08-26 01:31:51 +0530

    Add GitHub Actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // Base URL is automatically set by GitHub Actions
-const base = process.env.BASE_URL || '/';
+
 
 export default defineConfig({
-  base,
+  base :'/',
   plugins: [
     react(),
     sentryVitePlugin({
